adb3913dad50ef568b4607fb687097f346966d30
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-02-26 18:40:44 -0500

    Update MeanScores.c

--- MeanScores.c
+++ MeanScores.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 
 int main(void){
+    /* 
     int score1 = 72;
     int score2 = 73;
     int score3 = 43;
     int scores[3] = {score1, score2, score3};
+    */
+
+    /*    
+    int scores[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 43;
+    */
+
+    int num_scores;
+    int score;
 
     int sum = 0;
     float avg = 0;
-    int num_scores = sizeof(scores) / sizeof(scores[0]);
 
-    for (int i = 0; i < num_scores; i++) {
-        sum += scores[i];
+    printf("Enter # of Scores: ");
+    scanf("%d", &num_scores);
+    
+    int scores[num_scores];
+
+    for (int i = 0; i < num_scores; i++){
+        printf("Enter Score: ");
+        scanf("%d", &score);
+        scores[i] = score;
+        sum += score;
     }
 
     if (num_scores > 0) {
