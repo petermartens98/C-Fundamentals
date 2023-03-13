b8b2aa845d937303cb4645888ed9be71dad557c2
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-03-13 12:08:08 -0400

    Create

---
+++ CaeserCipher.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int shift, i;
+    char message[100], ch;
+    
+    printf("Enter a message to encrypt: ");
+    fgets(message, 100, stdin);  // read input string
+    
+    printf("Enter the shift amount: ");
+    scanf("%d", &shift);
+    
+    for(i = 0; message[i] != '\0'; i++) {
+        ch = message[i];
+        
+        if(ch >= 'a' && ch <= 'z') {
+            ch = 'a' + ((ch - 'a' + shift) % 26);  // apply shift to lowercase letter
+        }
+        else if(ch >= 'A' && ch <= 'Z') {
+            ch = 'A' + ((ch - 'A' + shift) % 26);  // apply shift to uppercase letter
+        }
+        
+        message[i] = ch;
+    }
+    
+    printf("Encrypted message: %s\n", message);
+    
+    return 0;
+}
